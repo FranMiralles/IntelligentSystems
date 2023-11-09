d9013c8764f2405bd42d1b67c8e7ecc6fe221722
Fran <fmiralles03@gmail.com>
2023-11-09 20:23:36 +0100

    Front Updates

--- SearchAlgMaze/interface.py
+++ SearchAlgMaze/interface.py
@@ -1,10 +1,28 @@
 import tkinter.ttk as ttk
 import tkinter as tk
-
+import os
+from PIL import Image, ImageTk
 
 # Instance of the window
 root = tk.Tk()
-root.title("Search Path in Maze")
+root.title("Searching Algorithms")
+
+root.geometry("1000x800+0+0")
+root.minsize(800, 500)
+
+# Menu walls
+menuBar = tk.Menu(root)
+menuBar.config(bg = '#ECECEC', font = ('Normal', 9))
+root.config(menu = menuBar, bg = '#ECECEC')
+presetMenu = tk.Menu(menuBar, tearoff=0)
+menuBar.add_cascade(label="Preset walls", menu=presetMenu)
+
+presetMenu.add_command(label="Preset #1")
+presetMenu.add_command(label="Preset #2")
+presetMenu.add_command(label="Preset #3")
+presetMenu.add_command(label="Preset #4")
+presetMenu.add_separator()
+presetMenu.add_command(label="Clear Walls")
 
 def pressButton(event):
     button = event.widget.button
@@ -17,8 +35,9 @@ def pressButton(event):
 
 # Frame1, maze
 
-frm1 = tk.Frame(root, padx=10, pady=10, bg='white')
-frm1.grid(row=0, column=0, padx= 5, pady= 5)
+frm1 = tk.LabelFrame(root, text="Maze", bg = '#ECECEC')
+#frm1.grid(row=0, column=0, padx= 5, pady= 5)
+frm1.place(relx=0.05, rely=0.05, relwidth=0.6, relheight=0.85)
 
 buttons = []
 
@@ -30,7 +49,8 @@ for i in range(15):
     row = []
     for j in range(15):
         button = tk.Button(frm1, text="", width=2, height=1)
-        button.grid(row=i, column=j)
+        button.place(relx=0.01 + j/15.5, rely= 0.01 + i/15.5, relheight=0.065, relwidth=0.065)
+        #button.grid(row=i, column=j)
         button.row = i
         button.col = j
         button.button = button
@@ -46,8 +66,10 @@ for i in range(15):
 
 # Frame2, select algorithms
 
-frm2 = tk.Frame(root, padx=10, pady=10, bg='white')
-frm2.grid(row=0, column=1, padx= 5, pady= 5)
+frm2 = tk.LabelFrame(root, padx=10, pady=10, text="Action", bg = '#ECECEC')
+#frm2.grid(row=0, column=1, padx= 5, pady= 5)
+frm2.place(relx=0.7, rely=0.1, relwidth=0.25, relheight=0.4)
+
 
 def getAlgorithmName():
     return comboBox.get()
@@ -60,8 +82,6 @@ def getSolveButoon():
 
 clearButton = tk.Button(frm2, text="Clear", width=4, height=1)
 clearButton.grid(row=1, pady=10)
-onlyPath = ttk.Checkbutton(frm2, text="Only Path")
-onlyPath.grid(row=1, column=1)
 
 def clearColors(event):
     for i in range(15):
@@ -79,3 +99,5 @@ comboBox.set(comboBox['values'][0])
 
 def loop():
     root.mainloop()
+
+root.mainloop()
